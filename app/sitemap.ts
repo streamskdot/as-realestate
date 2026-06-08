5953b749bc2babbaf438939994fe62569dcf0c8e
prismkoirala <prizamkoirala@gmail.com>
2026-06-08 07:10:19 +0545

    Add force-static to sitemap and robots for static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { SITE } from "@/lib/site-data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
     "",
